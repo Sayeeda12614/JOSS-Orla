4d62a70684f7cc8dc6829da58c0a49f8560a6233
Jessica <jesi7gmar@gmail.com>
2021-03-03 13:36:31 +0100

    plantilla

--- resources/views/plantilla-master.blade.php
+++ resources/views/plantilla-master.blade.php
@@ -15,35 +15,36 @@
     {{-- menu --}}
 @include('plantilla-partes.menu')
     {{-- contenido de prueba --}}
-        <div class="card-group">
-        <div class="card">
-          <img src="imagenes/logos/logo_favicon.png" height="280px" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
+    <div class="card-group">
+      <div class="card">
+        <img src="imagenes/logos/logo_favicon.png" height="280px" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
-        <div class="card">
-          <img src="imagenes/logos/fondo.jpg"  alt="..." height="280px">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-        <div class="card">
-          <img src="imagenes/logos/logo_favicon.png" height="280px" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>         
       </div>
+      <div class="card">
+        <img src="imagenes/logos/fondo.jpg"  alt="..." height="280px">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card">
+        <img src="imagenes/logos/logo_favicon.png" height="280px" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>         
+    </div>
       {{-- fin del contenido de prueba --}}
 
       @yield('content')
+      
 </body>
 @include('plantilla-partes.footer')
 
