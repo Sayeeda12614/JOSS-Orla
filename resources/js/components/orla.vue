eab2e3b46b23728f8419ea3c90bf850ff15de3ac
Sayeeda12614 <sayeeda.txurdi@gmail.com>
2021-03-09 23:34:15 +0100

    orla comenzada

---
+++ resources/js/components/orla.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+Vue.config.productionTip = false;
+export default {
+    data() {
+        
+    },
+}
+</script>
+
+<style>
+
+</style>
