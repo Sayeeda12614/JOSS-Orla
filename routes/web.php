07fd6ffd69009d252d816d3f57ec6998099ad99c
Jessica <jesi7gmar@gmail.com>
2021-03-15 16:27:32 +0100

    toOrla

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-//aquí iran todas las rutas
+
 Route::get('/', function () {
     return view('plantilla-master');
 });
