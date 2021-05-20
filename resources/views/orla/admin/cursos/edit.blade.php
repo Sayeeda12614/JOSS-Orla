040155972e22e308e6e70baa5433698337ef3c50
Jessica <jesi7gmar@gmail.com>
2021-05-20 12:25:43 +0200

    edit

--- resources/views/orla/admin/cursos/edit.blade.php
+++ resources/views/orla/admin/cursos/edit.blade.php
@@ -2,12 +2,22 @@
 @section('content')
     {{-- crear un nuevo curso --}}
     
-    <div class="content">
-        <form action="{{route('cursos.update', $curso->id)}}" method="POST">
+    <div class="content bg-dark text-white contentEditar">
+        <form class="formEdit" action="{{route('cursos.update', $curso->id)}}" method="POST">
             @csrf
             @method('PUT')
-            <input type="text" name="anio" value="{{$curso->anio}}">
-            <button type="submit">Enviar</button>
+
+            @error('nombre')
+                <div class="alert alert-danger">
+                    El nombre es obligatorio
+                </div>
+            @enderror
+            <div class="form-group">
+                <label for="exampleInputPassword1">Editar Curso</label>
+                <input type="text" name="anio" value="{{$curso->anio}}">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+            <a class="btn btn-warning" href="{{route('cursos.index')}}">Cancelar</a>
         </form>
 
     </div>
