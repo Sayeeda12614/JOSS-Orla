4c4a9e696912019ed76a56994cf767e8b22d40a8
Sayeeda12614 <sayeeda.txurdi@gmail.com>
2021-05-21 11:40:43 +0200

    menu

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,9 @@ Route::get('/', function () {
     return view('orla');
 })->name('inicio');
 
-// Auth::routes();
-
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Auth::routes();
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
