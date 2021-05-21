878c8127293533eff17377a34d9e69ef14c093e2
Ordoño Ndong <ncogo92@gmail.com>
2021-05-21 11:18:37 +0200

    Merged

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/', function () {
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+//Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
